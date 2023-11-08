615d8b27e8090e11bb14cdf601475527facf783f
calgor <ahomayoni3@gmail.com>
2023-11-08 11:35:56 +0330

    Add:added Astart

--- algo.py
+++ algo.py
@@ -1,16 +1,20 @@
 import numpy as np
 from state import next_state, solved_state
-from location import next_location
+from location import next_location, solved_location
 from util import util
 
 # from cube import cube
 
 
 class cube:
-    def __init__(self, state, cost, sequence) -> None:
+    def __init__(self, state, cost, sequence, location=None) -> None:
         self.state = state
         self.cost = cost
         self.sequence = sequence
+        self.location = location
+
+    def __iter__(self):
+        return iter(self.state)
 
 
 def solve(init_state, init_location, method):
@@ -39,7 +43,7 @@ def solve(init_state, init_location, method):
         return IDFS(init_state)
 
     elif method == "A*":
-        ...
+        return A_STAR(init_state, init_location)
 
     elif method == "BiBFS":
         ...
@@ -74,6 +78,79 @@ def IDFS(start_state):
                             )
                         )
         cost_limit += 1
+
+
+def A_STAR(start_state, start_location):
+    visited = dict()
+    actions = []
+    fringe = util.PriorityQueue()
+    expanded_nodes = 0
+    fringe.push(cube(start_state, 0, actions, start_location), 0)
+    while not fringe.isEmpty():
+        current_node = fringe.pop()
+        expanded_nodes += 1
+        if np.array_equal(current_node.state, solved_state()):
+            solution_info(visited, current_node.sequence, expanded_nodes)
+            return current_node.sequence
+        elif to_tuple(current_node.state) not in visited:
+            visited[to_tuple(current_node)] = current_node.cost
+            for i in range(12):
+                nextState = next_state(current_node.state, i + 1)
+                nextLocation = next_location(current_node.location, i + 1)
+                nextCost = heuristic(nextLocation) + len(actions) + 1
+                if to_tuple(nextState) not in visited:
+                    fringe.push(
+                        cube(
+                            nextState,
+                            nextCost,
+                            current_node.sequence + [i + 1],
+                            nextLocation,
+                        ),
+                        nextCost,
+                    )
+                else:
+                    base_state_to_current_node_cost = visited[to_tuple(nextState)]
+                    if nextCost < base_state_to_current_node_cost:
+                        fringe.update(
+                            cube(
+                                nextState,
+                                nextCost,
+                                current_node.sequence + [i + 1],
+                                nextLocation,
+                            ),
+                            nextCost,
+                        )
+
+    return []
+
+
+def heuristic(location):
+    solved_Location = solved_location()
+    total = 0
+    for i in range(2):
+        for j in range(2):
+            for k in range(2):
+                curr_value = location[i, j, k]
+                for x in range(2):
+                    for y in range(2):
+                        for z in range(2):
+                            if solved_Location[x, y, z] == curr_value:
+                                total += abs(x - i) + abs(y - j) + abs(z - k)
+                                break
+
+    return total / 4
+
+
+solution_info = lambda explored, actions, expanded_nodes: (
+    print(
+        "Depth of the solution path:",
+        len(actions),
+        "\nTotal number of expanded nodes:",
+        expanded_nodes,
+        "\nTotal number of nodes explored:",
+        len(explored),
+    )
+)
 
 
 # use this function to convert a numpy array to a tuple so that it can be used as a key in a dictionary or set
