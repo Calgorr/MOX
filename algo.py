51ffabbc07f1db866887e8f76c2afb4f9858c8d6
calgor <ahomayoni3@gmail.com>
2023-11-08 10:45:46 +0330

    Add: added BiBFS to algo.py

--- algo.py
+++ algo.py
@@ -1,17 +1,27 @@
 import numpy as np
 from state import next_state, solved_state
 from location import next_location
+from util import util
+
+# from cube import cube
+
+
+class cube:
+    def __init__(self, state, cost, sequence) -> None:
+        self.state = state
+        self.cost = cost
+        self.sequence = sequence
 
 
 def solve(init_state, init_location, method):
     """
     Solves the given Rubik's cube using the selected search algorithm.
- 
+
     Args:
         init_state (numpy.array): Initial state of the Rubik's cube.
         init_location (numpy.array): Initial location of the little cubes.
         method (str): Name of the search algorithm.
- 
+
     Returns:
         list: The sequence of actions needed to solve the Rubik's cube.
     """
@@ -22,17 +32,49 @@ def solve(init_state, init_location, method):
     # 3. use 'next_location()' to obtain the next location of the little cubes when taking an action.
     # 4. you can use 'Set', 'Dictionary', 'OrderedDict', and 'heapq' as efficient data structures.
 
-    if method == 'Random':
-        return list(np.random.randint(1, 12+1, 10))
-    
-    elif method == 'IDS-DFS':
-        ...
-    
-    elif method == 'A*':
+    if method == "Random":
+        return list(np.random.randint(1, 12 + 1, 10))
+
+    elif method == "IDS-DFS":
+        return IDFS(init_state)
+
+    elif method == "A*":
         ...
 
-    elif method == 'BiBFS':
+    elif method == "BiBFS":
         ...
-    
+
     else:
         return []
+
+
+def IDFS(start_state):
+    cost_limit = 1
+    while True:
+        fringe = util.Stack()
+        visited = set()
+        fringe.push(cube(start_state, 0, list()))
+        while not fringe.isEmpty():
+            current_node = fringe.pop()
+            if np.array_equal(current_node.state, solved_state()):
+                return current_node.sequence
+            if (
+                current_node.cost <= cost_limit
+                and to_tuple(current_node.state) not in visited
+            ):
+                visited.add(to_tuple(current_node.state))
+                for i in range(12):
+                    nextState = next_state(current_node.state, i + 1)
+                    if to_tuple(nextState) not in visited:
+                        fringe.push(
+                            cube(
+                                nextState,
+                                current_node.cost + 1,
+                                current_node.sequence + [i + 1],
+                            )
+                        )
+        cost_limit += 1
+
+
+def to_tuple(array):
+    return tuple(map(tuple, array))
---
+++ cube/cube.py
@@ -0,0 +1,5 @@
+class cube:
+    def __init__(self, state, cost, sequence) -> None:
+        self.state = state
+        self.cost = cost
+        self.sequence = sequence
